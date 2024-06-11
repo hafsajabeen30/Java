59183a1a061b380512503bd1c09c980bc47b1b03
Hafsa-jabeen <hafsajabeen30@gmail.com>
2024-06-11 11:22:05 +0200

    Commit 1
    Data types

---
+++ DataTypes.java
@@ -0,0 +1,10 @@
+public class DataTypes {
+    public static void main(String[] args){
+        String name = "Hafsa";
+        int age = 28;
+        double desiredSalary = 1200.50;
+        char gender = 'f';
+        boolean lookingForJob = false;
+        
+    }
+}
