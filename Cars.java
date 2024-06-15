c577179e646e7669cb643d99686d577b1680f54a
Hafsa-jabeen <hafsajabeen30@gmail.com>
2024-06-15 14:27:01 +0200

    Create Cars.java
    This shows the use of static and non static in classes and overall class concept.

---
+++ Cars.java
@@ -0,0 +1,35 @@
+public class Cars {
+
+    private static int totalCars= 0;
+    String make;
+    String model;
+    int year;
+
+    private Cars (String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        totalCars++;
+    }
+
+
+    public String printInfo(){
+        return " The make of the car is: " + this.make + "\n The model of the car is: " + this.model +  "\n The year is: " + this.year;
+    }
+
+    public static int getTotalCars() {
+        return totalCars;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(Cars.getTotalCars());
+        Cars myCar = new Cars("Tesla", "Model Y", 2019);
+        System.out.println(myCar.printInfo());
+        System.out.println(Cars.getTotalCars());
+        Cars yourCar = new Cars("Nisan", "Mocco", 2013);
+        System.out.println(yourCar.printInfo());
+        System.out.println(Cars.getTotalCars());
+    }
+
+}
