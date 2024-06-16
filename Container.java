d97740d3f2fcb5ac2448ce84b4faea0754ef9580
Hafsa-jabeen <hafsajabeen30@gmail.com>
2024-06-16 23:02:42 +0200

    Create Container.java
    Use of overload and arrays.

---
+++ Container.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+public class Container {
+
+    int[][] matrix;
+    int[] ages;
+    int id;
+    public Container(int[][] matrix, int[] ages, int id){
+        this.matrix = matrix;
+        this.ages = ages;
+        this.id = id;
+    }
+
+    public Container(int id){
+        this.matrix = new int[][] {{3,4},{3,5}};
+        this.ages = new int[] {3,4,5,6};
+        this.id = id;
+    }
+
+    public Container(){
+        this.matrix = new int[2][2];
+    }
+
+
+    @Override
+    public String toString(){
+        return "The matrix is: " + Arrays.deepToString(matrix) + " ,the ages are: " + Arrays.toString(ages) + " and the id is: " + id;
+
+    }
+
+    public static void main(String[] args){
+        Container myContainer = new Container(new int[][] {{2,3},{4,5}}, new int[] {22,33,44,55,66,77,88,99,21,20}, 22);
+        Container secContainer = new Container(10);
+        Container thirdContainer = new Container();
+        thirdContainer.matrix = new int[][] {{2,3}, {5,6}};
+        System.out.println("The matrix is: " + myContainer.toString());
+        System.out.println("The matrix is: " + secContainer.toString());
+        System.out.println("The matrix is: " + thirdContainer.toString());
+    }
+}
+
